234b7e8f3e7b5a7cde5d01c9524836da6f50a035
sinhvien <sinhvien>
2021-04-08 09:01:35 +0700

    them MVC project

---
+++ LabTH/LabTH/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LabTH.Startup))]
+namespace LabTH
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
